370e6a7c403731c874db592118bb8fc5f7b68028
Manuel de la Peña <mdelapenya@gmail.com>
2024-04-26 23:46:03 +0200

    chore: base app for building its binary

---
+++ Ch2/02_02/cmd/types.go
@@ -0,0 +1,119 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/AlecAivazis/survey/v2"
+
+	"github.com/linkedinlearning/domina-go/binaries/pokemon"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+}
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get [name]",
+	Short: "Get details about a Pokémon",
+	Args:  cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		ts, err := getTypes()
+		if err != nil {
+			log.Fatalf("failed to get types: %v", err)
+		}
+
+		types := make([]string, len(ts))
+		for i, t := range ts {
+			types[i] = t.Name
+		}
+
+		t := ""
+		prompt := &survey.Select{
+			Message: "¿Qué tipo de Pokémon buscas?",
+			Options: types,
+		}
+		survey.AskOne(prompt, &t)
+
+		pokemons, err := getPokemonsByType(t)
+		if err != nil {
+			log.Fatalf("failed to get pokemon: %v", err)
+		}
+
+		p := ""
+		prompt = &survey.Select{
+			Message: "¿Qué tipo de Pokémon buscas?",
+			Options: pokemons,
+		}
+		survey.AskOne(prompt, &p)
+
+		if err := getPokemon(p); err != nil {
+			log.Fatalf("failed to get pokemon: %v", err)
+		}
+	},
+}
+
+func getTypes() ([]pokemon.Type, error) {
+	resp, err := http.Get(fmt.Sprintf("https://pokeapi.co/api/v2/type"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to get pokemon: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	var search pokemon.TypeSearch
+	json.Unmarshal(body, &search)
+
+	return search.Results, nil
+}
+
+func getPokemonsByType(t string) ([]string, error) {
+	resp, err := http.Get(fmt.Sprintf("https://pokeapi.co/api/v2/type/%s", t))
+	if err != nil {
+		return nil, fmt.Errorf("failed to get pokemon: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	var search pokemon.PokemonByType
+	json.Unmarshal(body, &search)
+
+	var pokemons []string
+	for _, p := range search.Pokemon {
+		pokemons = append(pokemons, p.Pokemon.Name)
+	}
+
+	return pokemons, nil
+}
+
+func getPokemon(name string) error {
+	resp, err := http.Get(fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", name))
+	if err != nil {
+		return fmt.Errorf("failed to get pokemon: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	var pokemon pokemon.Pokemon
+	json.Unmarshal(body, &pokemon)
+	fmt.Printf("%s, I choose you!\n", pokemon.Name)
+
+	return nil
+}
---
+++ Ch2/02_02/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/linkedinlearning/domina-go/binaries/cmd"
+
+func main() {
+	cmd.Execute()
+}
