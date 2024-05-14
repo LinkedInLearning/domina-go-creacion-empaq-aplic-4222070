a41f97adea51ad780ecdebab9041e688cd577f84
Manuel de la Peña <mdelapenya@gmail.com>
2024-05-14 23:39:13 +0200

    chore: reorg web service directories

--- Ch1/01_06/main.go
+++ Ch1/01_06/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/linkedinlearning/domina-go/service/web"
-	"github.com/linkedinlearning/domina-go/service/web/middleware"
 )
 
 func main() {
@@ -16,32 +14,14 @@ func main() {
 }
 
 func run() error {
-	requiredToken := os.Getenv("API_TOKEN")
-
-	// We want to make sure the server token is set, bail if not
-	if requiredToken == "" {
-		log.Fatal("Please set API_TOKEN environment variable")
-	}
-
 	// Creates a router with default middleware:
 	// logger and recovery (crash-free) middleware
 	r := gin.Default()
-
-	initializeMiddleware(r)
 
 	initializeRoutes(r)
 
 	// By default, it listens on :8080
 	return r.Run()
-}
-
-func initializeMiddleware(r *gin.Engine) {
-	r.Use(
-		middleware.Counter(),   // contador de peticiones
-		middleware.Logging(),   // log de peticiones
-		middleware.AuthToken(), // autenticación
-		middleware.Error(),     // manejo de errores
-	)
 }
 
 func initializeRoutes(r *gin.Engine) {
--- Ch1/01_07/web/handlers.go
+++ Ch1/01_07/web/handlers.go
@@ -6,25 +6,34 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/linkedinlearning/domina-go/service/pokemon"
+	pokerror "github.com/linkedinlearning/domina-go/service/web/error"
 )
 
-func GetPokemon(r *gin.Context) {
-	name := r.Param("name")
+func GetPokemon(ctx *gin.Context) {
+	name := ctx.Param("name")
+	if name == "" {
+		err := pokerror.NewHttpError("Named parameter not found", "name parameter is required", http.StatusBadRequest)
+		ctx.Error(err)
+		return
+	}
+
 	p, err := pokemon.Get(name)
 	if err != nil {
-		r.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		err = pokerror.NewHttpError("Error getting pokemon", err.Error(), http.StatusInternalServerError)
+		ctx.Error(err)
 		return
 	}
 
-	r.JSON(http.StatusOK, gin.H{"pokemon": p})
+	ctx.JSON(http.StatusOK, gin.H{"pokemon": p})
 }
 
-func GetTypes(r *gin.Context) {
+func GetTypes(ctx *gin.Context) {
 	res, err := pokemon.GetTypes()
 	if err != nil {
-		r.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		err = pokerror.NewHttpError("Error getting pokemon types", err.Error(), http.StatusInternalServerError)
+		ctx.Error(err)
 		return
 	}
 
-	r.JSON(http.StatusOK, gin.H{"types": res})
+	ctx.JSON(http.StatusOK, gin.H{"types": res})
 }
